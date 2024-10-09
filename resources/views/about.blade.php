ffc91225e4809898e0b7b21176c99055fd6a3d91
Iffatunnisaa <iffatun@mhs.usk.ac.id>
2024-10-09 23:15:57 +0700

    New About us

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -6,18 +6,13 @@
 <div class="flex justify-center mt-32 mb-16">
     <h1 class="flex text-black text-4xl font-bold justify-center text-center">About Us</h1>
 </div>
-<p class="text-gray-700 text-center w-4/5 text-3xl mx-auto">Sabang, historically known as the "Golden Island", has been
-    an important trading centre and port since
-    ancient times. Important harbour since ancient times. The name "Sabang" comes from the Arabic word
-    for"erupting mountain", referring to the possibility of an active volcano in the past. The island was once
-    connected to Sumatra before separating, and was later given the name "Weh" which means "move" in
-    the Acehnese language.meaning "move" in Acehnese. Since the 12th century, Sabang has been an
-    important stopover for sailors such as the Sinbeks. For sailors such as Sinbad. During the Dutch colonial
-    era, Sabang became a vital coal port, and itscoal port, and it was even more important than Singapore
-    before the era of diesel ships. diesel ships. Although it has lost some of its significance since then, Sabang
-    remains an important point on international shipping routes. In international shipping lanes. With the
-    opening of the Free Trade Zone and Free Trade Zone and Free Port in 2000, Sabang again became a
-    vital logistics centre. Today,Sabang offers not only a rich historical heritage, but also stunning natural
-    beauty and unforgettable adventure opportunities. And unforgettable adventure opportunities for its visitors.
+<p class="text-gray-700 text-center w-4/5 text-3xl mx-auto">SabangTrip adalah platform digital yang dirancang untuk membantu 
+    wisatawan dalam merencanakan perjalanan ke Pulau Sabang, salah satu destinasi paling mempesona di Indonesia.
+    Melalui SabangTrip, Anda dapat dengan mudah menemukan berbagai destinasi wisata menarik di Sabang melalui 
+    fitur Discover, yang menampilkan informasi lengkap tentang tempat-tempat yang wajib dikunjungi. Kami juga 
+    menyediakan fitur e-ticket yang memudahkan Anda memesan tiket kapal secara online, sehingga perjalanan Anda 
+    lebih praktis dan nyaman. Selain itu, di halaman Testimoni, pengunjung dapat membaca atau berbagi pengalaman 
+    perjalanan mereka. SabangTrip berkomitmen untuk memberikan layanan yang memudahkan perjalanan Anda, menjadikan 
+    setiap kunjungan ke Sabang lebih berkesan dan menyenangkan.
 </p>
 @endsection
